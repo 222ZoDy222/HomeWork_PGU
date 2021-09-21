5c9a13779e3978b29c34d1ad14bf24fea4d028c1
222ZoDy222 <nik.gorbunov.02022002@mail.ru>
2021-09-21 21:43:55 +0300

    start 3th Lab

---
+++ lab_3/Задание 1/main.py
@@ -0,0 +1,80 @@
+import random
+M1 = []
+M2 = []
+N1 = int(input("First Matrix: "))
+N2 = int(input("Second Matrix: "))
+chance = [0,0,0,1,1,1,1,1,1,1]
+#Шанс 1 - 70%
+#Шанс 0 - 30%
+
+#Создание 2 матриц смежности неориентированных помеченных графов 
+# № 1
+for i in range(N1):
+	M1.append([])
+	for j in range(N1):
+		M1[i].append(random.choice(chance))
+		if(i == j):
+			M1[i][j] = 0
+		
+for i in range(N1):
+	for j in range(N1):
+		M1[i][j] = M1[j][i]
+		print(M1[i][j],end='\t')
+	print('\n')
+print('\n'*3)
+# № 2
+for i in range(N2):
+	M2.append([])
+	for j in range(N2):
+		M2[i].append(random.choice(chance))
+		if(i == j):
+			M2[i][j] = 0
+		
+for i in range(N1):
+	for j in range(N2):
+		M2[i][j] = M2[j][i]
+		print(M2[i][j],end='\t')
+	print('\n')
+print('\n'*3)
+# Преобразование в список смежности
+List_S1 = []
+print("Первый список смежности:\n")
+for i in range(N1):
+	List_S1.append([])
+	for j in range(N1):
+		if(M1[i][j] == 1):
+			List_S1[i].append(j+1)
+
+for i in range(N1):
+	print(i+1,' -- ',end='')
+	for j in range(len(List_S1[i])):
+		print(List_S1[i][j],end=' ')
+	print('\n')
+print("\n"*2)
+List_S2 = []
+print("Второй список смежности:\n")
+for i in range(N2):
+	List_S2.append([])
+	for j in range(N2):
+		if(M2[i][j] == 1):
+			List_S2[i].append(j+1)
+
+for i in range(N2):
+	print(i+1,' -- ',end='')
+	for j in range(len(List_S2[i])):
+		print(List_S2[i][j],end=' ')
+	print('\n')	
+print('\n'*3)
+
+
+
+
+
+
+
+
+
+
+
+
+
