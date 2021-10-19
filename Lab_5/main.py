f8c06821e7059c0fdef1ea9de9639dbe92c7969f
222ZoDy222 <nik.gorbunov.02022002@mail.ru>
2021-10-19 11:08:46 +0300

    WOW 4th lab again completed

--- Lab_5/main.py
+++ Lab_5/main.py
@@ -16,3 +16,27 @@ for i in range(N1):
 	print('\n')
 print('\n'*3)
 
+
+def BFS(v):
+	Vis[v] = 1
+	queue.append(v)
+	while(bool(queue)):
+		s = queue[0]
+		print(s+1)
+		queue.pop(0)
+		for i in range(len(M1)):
+			if(M1[s][i] == 1 and Vis[i] == 0):
+				queue.append(i)
+				Vis[i] = 1
+
+v = int(input('Вершина : ')) - 1
+
+Vis = []
+for i in range(len(M1)):
+	Vis.append(0)
+
+
+queue = []
+
+BFS(v)
+
