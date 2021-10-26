a8645930eb0570d8b3235cfde9e8a4075be5b1c9
222ZoDy222 <nik.gorbunov.02022002@mail.ru>
2021-10-26 10:39:02 +0300

    Completed 5th lab

--- Lab_5/main.py
+++ Lab_5/main.py
@@ -29,6 +29,33 @@ def BFS(v):
 				queue.append(i)
 				Vis[i] = 1
 
+
+
+print("---список смежности---")
+
+for i in range(len(G)):
+    c.append([]) 
+    for j in range(len(G[i])):
+        if(G[i][j] == 1):
+            c[i].append(j)
+    print("\n",i+1," - ",end='')
+    for j in range(len(c[i])):
+        print(c[i][j]+1, end=' ')
+
+def BFS1(v1):
+	Vis1[v1] = 1
+	queue1.append(v1)
+	while(bool(queue1)):
+		l = queue1[0]
+		print(l+1)
+		queue1.pop(0)
+		for i in range(len(c[l])):
+			if(Vis1[c[l][i]] == 0):
+				queue1.append(c[l][i])
+				Vis1[c[l][i]] = 1
+
+
+
 v = int(input('Вершина : ')) - 1
 
 Vis = []
@@ -39,4 +66,19 @@ for i in range(len(M1)):
 queue = []
 
 BFS(v)
+
+
+print('\n')
+
+v1 = int(input('Вершина : ')) - 1
+
+Vis1 = []
+for i in range(len(c)):
+	Vis1.append(0)
+
+queue1 = []
+
+BFS1(v1)
+
+input()
 
